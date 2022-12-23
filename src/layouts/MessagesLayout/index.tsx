96a202fbcedd0f95382015c622302dbac601f664
Steven <steven@zhao.io>
2022-12-22 21:02:17 -0800

    ui: listen to dms

--- src/layouts/MessagesLayout/index.tsx
+++ src/layouts/MessagesLayout/index.tsx
@@ -1,55 +1,57 @@
-import React, { useEffect, useState } from "react";
-import Message from "../../components/Message";
-import MessagesNav from "../../components/MessagesNav";
+import { listen } from "@tauri-apps/api/event";
 import { invoke } from "@tauri-apps/api/tauri";
 
-const MessagesLayout = ({
-  user_profile,
-  user,
-  peer,
-  setPeer,
-}) => {
+import { useEffect, useState } from "react";
+import Message from "../../components/Message";
+
+import MessagesNav from "../../components/MessagesNav";
+
+const MessagesLayout = ({ user_profile, user, peer, setPeer }) => {
   const [conversation, setConversation] = useState([]);
   const [message_list, setMessageList] = useState([]);
   const [peer_profiles, setPeerProfiles] = useState({});
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const getMessages = async () => {
-      return await invoke("user_convos", { privkey: user });
-    };
-    
-    const getProfiles = async (messages) => {
-      const profiles = {};
-      for (const message of messages) {
-        try {
-          const profile:any = await invoke("user_profile", { pubkey: message.peer });
-          if (!profile.picture) {
-            profile.picture = `https://robohash.org/${message.peer}.png`;
-          }
-          profiles[message.peer] = profile;
-        } catch {
-          console.log("failed to get profile for ", message.peer);
+  const getMessages = async () => {
+    return await invoke("user_convos", { privkey: user });
+  };
 
-          profiles[message.peer] = {
-            picture: `https://robohash.org/${message.peer}.png`,
-            pubkey: message.peer
-          };
+  const getProfiles = async (messages) => {
+    const profiles = {};
+    for (const message of messages) {
+      try {
+        const profile: any = await invoke("user_profile", {
+          pubkey: message.peer,
+        });
+        if (!profile.picture) {
+          profile.picture = `https://robohash.org/${message.peer}.png`;
         }
-      }
-      return profiles;
-    };
+        profiles[message.peer] = profile;
+      } catch {
+        console.log("failed to get profile for ", message.peer);
 
+        profiles[message.peer] = {
+          picture: `https://robohash.org/${message.peer}.png`,
+          pubkey: message.peer,
+        };
+      }
+    }
+    return profiles;
+  };
+
+  const refreshMessages = () =>
     getMessages()
       .then((messages: any) => {
-        return getProfiles(messages).then((profiles) => {
-          console.log("conversation", messages);
-          setMessageList(messages);
-          setPeerProfiles(profiles);
-          setLoading(false);
-        }).catch(e => {
-          console.log(e);
-        });
+        return getProfiles(messages)
+          .then((profiles) => {
+            console.log("conversation", messages);
+            setMessageList(messages);
+            setPeerProfiles(profiles);
+            setLoading(false);
+          })
+          .catch((e) => {
+            console.log(e);
+          });
       })
       .catch((e) => {
         setMessageList([]);
@@ -57,6 +59,20 @@ const MessagesLayout = ({
         setLoading(false);
         console.log(e);
       });
+
+  useEffect(() => {
+    const unlisten = listen("dm", (event) => {
+      // event.event is the event name (useful if you want to use a single callback fn for multiple event types)
+      // event.payload is the payload object
+      // refreshMessages();
+      console.log(event);
+    });
+
+    refreshMessages();
+
+    return () => {
+      unlisten.then((f) => f());
+    };
   }, []);
 
   useEffect(() => {
@@ -83,9 +99,22 @@ const MessagesLayout = ({
   return (
     <>
       <MessagesNav
-        {...{ message_list, peer_profile: peer_profiles[peer], peer_profiles, setPeer }}
+        {...{
+          message_list,
+          peer_profile: peer_profiles[peer],
+          peer_profiles,
+          setPeer,
+        }}
       />
-        <Message {...{ user_profile, peer_profile: peer_profiles[peer], peer, conversation, loading }} />
+      <Message
+        {...{
+          user_profile,
+          peer_profile: peer_profiles[peer],
+          peer,
+          conversation,
+          loading,
+        }}
+      />
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,23 @@
-import { NostrProvider } from "@nostrgg/react";
 import { invoke } from "@tauri-apps/api/tauri";
+import Link from "next/link";
 import { useState } from "react";
-import Navigation from "../components/Navigation";
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import Layout from "../layouts/Layout";
 
 const relayUrls = [
   "wss://nostr-pub.wellorder.net",
   "wss://nostr-relay.untethr.me",
 ];
 
-function App({peer, setPeer, user, setUser}) {
+function App({ peer, setPeer, user, setUser }) {
   const [greetMsg, setGreetMsg] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("user_profile", { pubkey: '6a0e6b709fb5239a3df637695764f153e56edccd48fa1c64916b8481d0ca3ab3' }));
+    setGreetMsg(
+      await invoke("user_profile", {
+        pubkey:
+          "6a0e6b709fb5239a3df637695764f153e56edccd48fa1c64916b8481d0ca3ab3",
+      })
+    );
   }
 
   // const user = {
@@ -29,27 +30,50 @@ function App({peer, setPeer, user, setUser}) {
   //   cli: "da43ff28ba49aad308de30426c16c106beb25a4b381b36e2e72c64f3e6b8a3ee"
   // }
 
-  const privkey="bb13681e0cd2f86d6a8d124fe051abf8a8a250a6d7357cb6b6e67a8640203ece";
+  const privkey =
+    "bb13681e0cd2f86d6a8d124fe051abf8a8a250a6d7357cb6b6e67a8640203ece";
 
   return (
     <div className="bg-neutral-900 w-full h-[100vh]">
-    <h1 className="flex justify-center text-4xl pt-2 bg-neutral-800">debug menu</h1>
+      <h1 className="flex justify-center text-4xl pt-2 bg-neutral-800">
+        debug menu
+      </h1>
 
-    <div className="flex flex-row px-5 pt-5">
-    <input className="rounded-lg px-2 bg-slate-600" id="pubkeyinput" type="text" value={user} onChange={(e) => setUser(e.target.value)} placeholder="Enter private key"/>
-    <Link href={"/messages?privkey=" + user} >
-      <h2 className="px-4 mx-2 cursor-pointer bg-neutral-800 hover:bg-slate-700 rounded-md">Launch</h2>
-    </Link>
-    </div>
+      <div className="flex flex-row px-5 pt-5">
+        <input
+          className="rounded-lg px-2 bg-slate-600"
+          id="pubkeyinput"
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          placeholder="Enter private key"
+        />
+        <Link href={"/messages?privkey=" + user}>
+          <h2
+            className="px-4 mx-2 cursor-pointer bg-neutral-800 hover:bg-slate-700 rounded-md"
+            onClick={(e) =>
+              invoke("sub_to_msg_events", {
+                privkey: user,
+              })
+            }
+          >
+            Launch
+          </h2>
+        </Link>
+      </div>
 
+      <div className="flex p-5">
+        <Link href={`/messages?privkey=${privkey}`}>
+          <h2
+            className="rounded-lg px-2 bg-neutral-800 hover:bg-slate-700 cursor-pointer"
+            onClick={(e) => setUser(privkey)}
+          >
+            Launch with default private key
+          </h2>
+        </Link>
+      </div>
 
-    <div className="flex p-5">
-    <Link href={`/messages?privkey=${privkey}`} >
-      <h2 className="rounded-lg px-2 bg-neutral-800 hover:bg-slate-700 cursor-pointer" onClick={(e) => setUser(privkey)}>Launch with default private key</h2>
-    </Link>
-    </div>
-
-    {/* <button onClick={greet}>test greet</button>
+      {/* <button onClick={greet}>test greet</button>
     <p>{JSON.stringify(greetMsg)}</p> */}
     </div>
   );
