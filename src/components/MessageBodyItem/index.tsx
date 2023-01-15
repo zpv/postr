fe391d472f4547515c70dbb65899f5ac85326e53
Kelvin Zhao <icyaneon@gmail.com>
2023-01-14 18:30:14 -0800

    feat: add expand button for long messages

--- src/components/MessageBodyItem/index.tsx
+++ src/components/MessageBodyItem/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { getTime } from "../../helpers/timeHelpers";
 
 interface MessageBodyItemProps {
@@ -17,10 +18,9 @@ const MessageBodyItem: React.FC<MessageBodyItemProps> = ({
   rounded_bottom,
   rounded_top,
 }) => {
-  if (content === "") return <></>;
-
+  const MAX_LENGTH: number = 200;
+  const TRIM_LENGTH: number = 50;
   const isUser: boolean = author === user;
-
   const userStyling: string =
     "bg-indigo-800 rounded-bl-2xl rounded-tl-2xl" +
     ((rounded_bottom && " rounded-br-2xl") || "") +
@@ -29,8 +29,10 @@ const MessageBodyItem: React.FC<MessageBodyItemProps> = ({
     "bg-neutral-800 rounded-br-2xl rounded-tr-2xl" +
     ((rounded_bottom && " rounded-bl-2xl") || "") +
     ((rounded_top && " rounded-tl-2xl") || "");
-
   const timeStr: string = getTime(timestamp);
+  const [expanded, setExpanded] = useState(false);
+
+  if (content === "") return <></>;
 
   return (
     <>
@@ -48,7 +50,20 @@ const MessageBodyItem: React.FC<MessageBodyItemProps> = ({
             (isUser ? userStyling : authorStyling)
           }
         >
-          {content}
+          {(content.length <= MAX_LENGTH || expanded) && content}
+          {content.length > MAX_LENGTH && !expanded && (
+            <>
+              {content.slice(0, MAX_LENGTH - TRIM_LENGTH)}...
+              <span
+                className="cursor-pointer text-indigo-300 underline hover:no-underline"
+                onClick={() => setExpanded(true)}
+              >
+                <br />
+                Expand {content.length - MAX_LENGTH + TRIM_LENGTH} more
+                characters
+              </span>
+            </>
+          )}
         </p>
         {!isUser && (
           <p className="invisible my-auto mr-5 text-center text-xs text-neutral-400 group-hover:visible">
